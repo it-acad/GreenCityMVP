796c580869d4aefae882be0c8db19e90de924f29
SonnyRose <keeganwunder0@gmail.com>
2024-08-08 16:29:03 +0300

    changing all received data in tests on JSON format

--- core/src/test/java/greencity/controller/HabitControllerTest.java
+++ core/src/test/java/greencity/controller/HabitControllerTest.java
@@ -15,8 +15,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
-import org.mockito.quality.Strictness;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 import org.springframework.data.domain.PageRequest;
@@ -44,7 +42,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @ExtendWith(MockitoExtension.class)
-@MockitoSettings(strictness = Strictness.LENIENT)
 public class HabitControllerTest {
 
     @InjectMocks
