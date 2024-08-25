a82e39d534a54d7fdc39f7a2ad4607208e516d42
zenuk <zenukddd@gmail.com>
2024-08-25 21:00:38 +0200

    Added swagger documentation to NotificationController

--- core/src/main/java/greencity/controller/NotificationController.java
+++ core/src/main/java/greencity/controller/NotificationController.java
@@ -1,10 +1,14 @@
 package greencity.controller;
 
 import greencity.annotations.CurrentUser;
+import greencity.constant.HttpStatuses;
 import greencity.dto.notification.NotificationDto;
 import greencity.dto.user.UserVO;
 import greencity.service.NotificationService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +21,18 @@ import java.util.List;
 public class NotificationController {
     private final NotificationService notificationService;
 
+    /**
+     * Method for getting all unread notifications for current user.
+     *
+     * @return list of {@link NotificationDto} instances.
+     * @author Zenovii Dudak.
+     */
+    @Operation(summary = "Get all unread notifications.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
+            @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED),
+            @ApiResponse(responseCode = "404", description = HttpStatuses.NOT_FOUND)
+    })
     @GetMapping("/unread")
     public ResponseEntity<List<NotificationDto>> getUnreadNotifications(
             @Parameter(hidden = true) @CurrentUser UserVO currentUser) {
@@ -24,6 +40,18 @@ public class NotificationController {
         return ResponseEntity.ok(notifications);
     }
 
+    /**
+     * Method for getting all notifications for current user.
+     *
+     * @return list of {@link NotificationDto} instances.
+     * @author Zenovii Dudak.
+     */
+    @Operation(summary = "Get all notifications.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
+            @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED),
+            @ApiResponse(responseCode = "404", description = HttpStatuses.NOT_FOUND)
+    })
     @GetMapping("/all")
     public ResponseEntity<List<NotificationDto>> getAllNotifications(
             @Parameter(hidden = true) @CurrentUser UserVO currentUser) {
@@ -31,12 +59,37 @@ public class NotificationController {
         return ResponseEntity.ok(notifications);
     }
 
+    /**
+     * Method for marking notification as viewed.
+     *
+     * @param id - notification id.
+     * @return void.
+     * @author Zenovii Dudak.
+     */
+    @Operation(summary = "Marking notification as viewed.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
+            @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED),
+            @ApiResponse(responseCode = "404", description = HttpStatuses.NOT_FOUND)
+    })
     @PostMapping("/markAsViewed/{id}")
     public ResponseEntity<Void> markAsViewed(@PathVariable Long id) {
         notificationService.markAsReadNotification(id);
         return ResponseEntity.ok().build();
     }
 
+    /**
+     * Method for getting first three notifications for current user.
+     *
+     * @return list of {@link NotificationDto} instances.
+     * @author Dmytro Lysenko.
+     */
+    @Operation(summary = "Getting first three notifications.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
+            @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED),
+            @ApiResponse(responseCode = "404", description = HttpStatuses.NOT_FOUND)
+    })
     @GetMapping()
     public ResponseEntity<List<NotificationDto>> getFirstThreeNotifications(
             @Parameter(hidden = true) @CurrentUser UserVO currentUser) {
@@ -44,6 +97,19 @@ public class NotificationController {
         return ResponseEntity.ok(notifications);
     }
 
+    /**
+     * Method for getting all notifications for current user sorted by received time.
+     *
+     * @param ascending - if true, notifications will be sorted in ascending order, otherwise in descending.
+     * @return list of {@link NotificationDto} instances.
+     * @author Dmytro Lysenko.
+     */
+    @Operation(summary = "Getting sorted notification by received time.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
+            @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED),
+            @ApiResponse(responseCode = "404", description = HttpStatuses.NOT_FOUND)
+    })
     @GetMapping("/sorted")
     public ResponseEntity<List<NotificationDto>> getNotificationsSortedByReceivedTime(
             @Parameter(hidden = true) @CurrentUser UserVO currentUser,
