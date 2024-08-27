cf7a89fb3438fecf94cc8e48c9aa6d99eeef6613
vulook <andriy.dep@gmail.com>
2024-08-27 14:26:42 +0300

    add /events/create" -> SecurConfig

--- core/src/main/java/greencity/config/SecurityConfig.java
+++ core/src/main/java/greencity/config/SecurityConfig.java
@@ -204,6 +204,8 @@ public class SecurityConfig {
                                 "/econews/dislike",
                                 "/econews/comments/{econewsId}",
                                 "/econews/comments/like",
+                                "/events",
+                                "/events/create",
                                 CUSTOM_SHOPPING_LIST_ITEMS,
                                 "/files/image",
                                 "/files/convert",
