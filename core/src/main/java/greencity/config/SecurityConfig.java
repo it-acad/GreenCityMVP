8b98df1ad979103ea780d0cc061444ff52ad2e3b
DmitryKhomich <94125137+DmitryKhomich@users.noreply.github.com>
2024-09-03 16:44:17 +0300

    [161,163] update edit endpoint according to new fields in Event entity

--- core/src/main/java/greencity/config/SecurityConfig.java
+++ core/src/main/java/greencity/config/SecurityConfig.java
@@ -288,6 +288,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET,
                                 "/events/{userID}")
                         .hasAnyRole(ADMIN, USER)
+                        .anyRequest().hasAnyRole(ADMIN)
+                        .anyRequest().hasAnyRole(ADMIN)
                 )
                 .logout(logout -> logout.logoutUrl("/logout")
                         .logoutRequestMatcher(new AntPathRequestMatcher("/management/logout", "GET"))
