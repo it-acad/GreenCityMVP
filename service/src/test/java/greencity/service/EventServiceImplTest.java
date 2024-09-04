5199ecef1c54630abbc4a84854697daa0f016113
Chernenko Vitaliy <chernenko_vitaliy@ukr.net>
2024-09-04 17:34:34 +0300

    Added units for savingEvent method for EventServiceImpl;

--- service/src/test/java/greencity/service/EventServiceImplTest.java
+++ service/src/test/java/greencity/service/EventServiceImplTest.java
@@ -1,38 +1,65 @@
 package greencity.service;
 
 import greencity.ModelUtils;
+import greencity.client.RestClient;
+import greencity.constant.ErrorMessage;
+import greencity.dto.event.EventCreationDtoRequest;
 import greencity.dto.event.EventDto;
 import greencity.dto.event.EventEditDto;
 import greencity.dto.user.AuthorDto;
+import greencity.dto.user.PlaceAuthorDto;
 import greencity.entity.Event;
 import greencity.entity.EventDayDetails;
 import greencity.entity.User;
 import greencity.exception.exceptions.EventNotFoundException;
+import greencity.exception.exceptions.NotFoundException;
+import greencity.exception.exceptions.NotSavedException;
 import greencity.repository.EventRepo;
+import greencity.repository.UserRepo;
+import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.security.Principal;
 import java.util.*;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class EventServiceImplTest {
+    private static final Long EVENT_ID = 1L;
+    private static final Long USER_ID = 1L;
 
     @Mock
     private EventRepo eventRepo;
+
+    @Mock
+    private UserRepo userRepo;
+
     @Mock
     private ModelMapper modelMapper;
+
+    @Mock
+    private FileService fileService;
+
+    @Mock
+    private HttpServletRequest httpServletRequest;
+
+    @Mock
+    private RestClient restClient;
+
     @InjectMocks
     private EventServiceImpl eventService;
-    private static final Long EVENT_ID = 1L;
-    private static final Long USER_ID = 1L;
+
 
     @Test
     void testDeleteEvent_Successful() {
@@ -139,5 +166,54 @@ class EventServiceImplTest {
         Set<EventDto> result = eventService.findAll();
 
         assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void saveEvent_AllDataValid_ReturnEventDto() {
+        List<MultipartFile> images = new ArrayList<>(List.of(
+                new MultipartFileImpl("image1.png", "image1.png", "image/png", new byte[]{}),
+                new MultipartFileImpl("image2.png", "image2.png", "image/png", new byte[]{}),
+                new MultipartFileImpl("image3.png", "image3.png", "image/png", new byte[]{})));
+        Principal currentUser = ModelUtils.getPrincipal();
+        EventCreationDtoRequest eventCreationDtoRequest = ModelUtils.getEventCreationDto();
+        Event event = ModelUtils.getEvent();
+        PlaceAuthorDto placeAuthorDto = PlaceAuthorDto.builder().id(1L).name("Name").email(currentUser.getName()).build();
+        when(userRepo.findByEmail(currentUser.getName())).thenReturn(Optional.of(ModelUtils.getUser()));
+        when(modelMapper.map(eventCreationDtoRequest, Event.class)).thenReturn(event);
+        when(eventRepo.save(event)).thenReturn(event);
+        when(fileService.upload(any(MultipartFile.class))).thenReturn("https://someimageurlpath.com");
+        when(httpServletRequest.getHeader("Authorization")).thenReturn("some_security_tocken");
+        when(modelMapper.map(event.getAuthor(), PlaceAuthorDto.class)).thenReturn(placeAuthorDto);
+        doNothing().when(restClient).addEvent(any());
+        when(modelMapper.map(event, EventDto.class)).thenReturn(ModelUtils.getEventDto());
+
+
+        EventDto result = eventService.saveEvent(ModelUtils.getEventCreationDto(), images, currentUser.getName());
+
+        assertNotNull(result);
+    }
+
+    @Test
+    public void saveEvent_UserEmailNotExists_ThrowException() {
+        String notExistsEmail = "notExistsEmail@some.com";
+        when(userRepo.findByEmail(notExistsEmail)).thenThrow(new NotFoundException(ErrorMessage.USER_NOT_FOUND_BY_EMAIL + notExistsEmail));
+
+        assertThatThrownBy(() -> eventService.saveEvent(ModelUtils.getEventCreationDto(), new ArrayList<>(), notExistsEmail))
+                .isInstanceOf(NotFoundException.class);
+    }
+
+    @Test
+    public void saveEvent_EventNotValid_ThrowException() {
+        Event notValidEvent = ModelUtils.getEvent();
+        notValidEvent.setEventType(null);
+        notValidEvent.setEventTitle(null);
+        notValidEvent.setAuthor(null);
+        when(userRepo.findByEmail(ModelUtils.getPrincipal().getName())).thenReturn(Optional.of(ModelUtils.getUser()));
+        when(modelMapper.map(ModelUtils.getEventCreationDto(), Event.class)).thenReturn(notValidEvent);
+        when(eventRepo.save(notValidEvent)).thenThrow(new DataIntegrityViolationException("Some error message"));
+
+
+        assertThatThrownBy(() -> eventService.saveEvent(ModelUtils.getEventCreationDto(), new ArrayList<>(), ModelUtils.getPrincipal().getName()))
+                .isInstanceOf(NotSavedException.class);
     }
 }
