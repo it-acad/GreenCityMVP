6101dd962dbce846fc4bebb34e106b78334cf430
zenuk <zenukddd@gmail.com>
2024-09-05 14:55:41 +0200

    Added swagger documentation to the ReplyToCommentController

--- core/src/main/java/greencity/controller/ReplyToCommentController.java
+++ core/src/main/java/greencity/controller/ReplyToCommentController.java
@@ -1,10 +1,16 @@
 package greencity.controller;
 
 import greencity.annotations.CurrentUser;
+import greencity.constant.HttpStatuses;
 import greencity.dto.replytocomment.ReplyToCommentDto;
 import greencity.dto.user.UserVO;
 import greencity.service.ReplyToCommentService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,6 +26,20 @@ public class ReplyToCommentController {
 
     private final ReplyToCommentService replyToCommentService;
 
+    /**
+     * Method for saving reply to comment.
+     *
+     * @param commentId id of comment
+     * @return {@link ReplyToCommentDto}
+     */
+    @Operation(summary = "Save reply to comment")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = HttpStatuses.CREATED,
+                    content = @Content(schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "400", description = HttpStatuses.BAD_REQUEST),
+            @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED)
+    })
     @PostMapping("{commentId}")
     public ResponseEntity<ReplyToCommentDto> save(@PathVariable("commentId") Long commentId,
                                                   @Valid @RequestBody ReplyToCommentDto replyToCommentDto,
@@ -28,6 +48,19 @@ public class ReplyToCommentController {
         return ResponseEntity.status(HttpStatus.CREATED).body(savedReply);
     }
 
+    /**
+     * Method for updating reply to comment.
+     *
+     * @return {@link ReplyToCommentDto}
+     */
+    @Operation(summary = "Update reply to comment")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = HttpStatuses.OK,
+                    content = @Content(schema = @Schema(implementation = ReplyToCommentDto.class))),
+            @ApiResponse(responseCode = "400", description = HttpStatuses.BAD_REQUEST),
+            @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED),
+            @ApiResponse(responseCode = "403", description = HttpStatuses.FORBIDDEN)
+    })
     @PatchMapping()
     public ResponseEntity<ReplyToCommentDto> update(@Valid @RequestBody ReplyToCommentDto replyToCommentDto,
                                                     @Parameter(hidden = true) @CurrentUser UserVO currentUser) {
@@ -35,12 +68,35 @@ public class ReplyToCommentController {
         return ResponseEntity.ok(updatedReply);
     }
 
+    /**
+     * Method for deleting reply to comment.
+     *
+     *
+     */
+    @Operation(summary = "Delete reply to comment")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = HttpStatuses.BAD_REQUEST),
+            @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED),
+            @ApiResponse(responseCode = "403", description = HttpStatuses.FORBIDDEN)
+    })
     @DeleteMapping()
     public void delete(@RequestParam Long replyToCommentId,
                        @Parameter(hidden = true) @CurrentUser UserVO currentUser) {
         replyToCommentService.deleteById(replyToCommentId, currentUser.getId());
     }
 
+    /**
+     * Method for getting all replies to comment.
+     *
+     * @return {@link List<ReplyToCommentDto>}
+     */
+    @Operation(summary = "Get all replies to comment")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = HttpStatuses.OK,
+                    content = @Content(schema = @Schema(implementation = ReplyToCommentDto.class))),
+            @ApiResponse(responseCode = "400", description = HttpStatuses.BAD_REQUEST),
+            @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED)
+    })
     @GetMapping("/allReplies/{commentId}")
     public ResponseEntity<List<ReplyToCommentDto>> getAll(@PathVariable Long commentId) {
         return ResponseEntity.ok(replyToCommentService.findAllByCommentId(commentId));
