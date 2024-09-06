3a384cf5b798b2827cc2ac69a486ffa000ff0f61
SonnyRose <keeganwunder0@gmail.com>
2024-09-06 14:25:15 +0300

    fixed some bugs

--- service/src/main/java/greencity/service/NotificationServiceImpl.java
+++ service/src/main/java/greencity/service/NotificationServiceImpl.java
@@ -3,19 +3,19 @@ package greencity.service;
 import greencity.constant.ErrorMessage;
 import greencity.dto.notification.NotificationDto;
 import greencity.entity.Notification;
-import greencity.exception.exceptions.BadRequestException;
-import greencity.exception.exceptions.InvalidUserIdException;
-import greencity.exception.exceptions.NotificationNotFoundException;
+import greencity.exception.exceptions.*;
 import greencity.mapping.NotificationMapper;
 import greencity.repository.NotificationRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -26,6 +26,7 @@ public class NotificationServiceImpl implements NotificationService {
     @Override
     @Transactional
     public NotificationDto save(NotificationDto notificationDto) {
+        validateNotification(notificationDto);
         Notification mappedNotification = mapper.toEntity(notificationDto);
         Notification savedNotification = notificationRepo.save(mappedNotification);
         return mapper.toDto(savedNotification);
@@ -33,6 +34,7 @@ public class NotificationServiceImpl implements NotificationService {
 
     @Override
     public List<NotificationDto> findAllByUserId(Long userId) {
+        validateId(userId);
         List<Notification> notifications = notificationRepo.findAllByUserId(userId);
         return notifications.stream()
                 .map(mapper::toDto)
@@ -41,9 +43,7 @@ public class NotificationServiceImpl implements NotificationService {
 
     @Override
     public List<NotificationDto> findAllByUserIdAndIsReadFalse(Long userId) {
-        if (userId == null) {
-            throw new InvalidUserIdException("User ID cannot be null");
-        }
+        validateId(userId);
         List<Notification> notifications = notificationRepo.findAllByUserIdAndIsReadFalse(userId);
         if (notifications.isEmpty()){
             return Collections.emptyList();
@@ -57,7 +57,7 @@ public class NotificationServiceImpl implements NotificationService {
     @Transactional
     public void markAsReadNotification(Long id) {
         Notification notification = notificationRepo.findById(id)
-                .orElseThrow(() -> new NotificationNotFoundException("Notification with ID " + id + " not found"));
+                .orElseThrow(() -> new NotificationNotFoundException(ErrorMessage.NOTIFICATION_NOT_FOUND_BY_ID + id));
 
         if (notification.isRead()){
             throw new BadRequestException(ErrorMessage.NOTIFICATION_ALREADY_READ);
@@ -68,6 +68,7 @@ public class NotificationServiceImpl implements NotificationService {
 
     @Override
     public List<NotificationDto> getFirstThreeNotifications(Long userId) {
+        validateId(userId);
         List<Notification> notifications = notificationRepo.findFirstThreeByUserIdOrderByReceivedTimeDesc(userId);
         return notifications.stream()
                 .map(mapper::toDto)
@@ -76,6 +77,7 @@ public class NotificationServiceImpl implements NotificationService {
 
     @Override
     public List<NotificationDto> getNotificationsSortedByReceivedTime(Long userId, boolean ascending) {
+        validateId(userId);
         List<Notification> notifications = notificationRepo.findAllByUserIdOrderByReceivedTimeDesc(userId);
         Comparator<Notification> comparator = Comparator.comparing(Notification::getReceivedTime);
 
@@ -86,5 +88,29 @@ public class NotificationServiceImpl implements NotificationService {
                 .sorted(comparator)
                 .map(mapper::toDto)
                 .toList();
+    }
+
+    public boolean isOwner(Long notificationId, Long userId) {
+        Notification notification = notificationRepo.findById(notificationId)
+                .orElseThrow(() -> new NotFoundException(ErrorMessage.NOTIFICATION_NOT_FOUND_BY_ID + notificationId));
+        return notification.getUser().getId().equals(userId);
+    }
+
+    private void validateNotification(NotificationDto notificationDto) {
+        Objects.requireNonNull(notificationDto, ErrorMessage.NOTIFICATION_DTO_CANNOT_BE_NULL);
+
+        if (!StringUtils.hasText(notificationDto.getSection())) {
+            throw new InvalidNotificationException(ErrorMessage.NOTIFICATION_SECTION_CANNOT_BE_NULL_OR_EMPTY);
+        }
+        if (!StringUtils.hasText(notificationDto.getSectionType())) {
+            throw new InvalidNotificationException(ErrorMessage.NOTIFICATION_SECTION_TYPE_CANNOT_BE_NULL_OR_EMPTY);
+        }
+        if (!StringUtils.hasText(notificationDto.getText())) {
+            throw new InvalidNotificationException(ErrorMessage.NOTIFICATION_TEXT_CANNOT_BE_NULL_OR_EMPTY);
+        }
+    }
+
+    private void validateId(Long id) {
+        Objects.requireNonNull(id, ErrorMessage.USER_ID_CANNOT_BE_NULL);
     }
 }
